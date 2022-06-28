46131ec13b1783029d579299b50ad0929a5b6b2a
sedhiem <sedhiem@gmail.com>
2022-06-28 11:53:11 +0900

    Update: v3/coordinatorRouter2

--- src/v3/coordinatorRouter2.m
+++ src/v3/coordinatorRouter2.m
@@ -10,6 +10,7 @@ classdef coordinatorRouter2 < matlab.DiscreteEventSystem
     end
 
     properties(Nontunable)
+        Capacity = 1;
         routerID = 1;
         delay = 1;
     end
@@ -25,8 +26,8 @@ classdef coordinatorRouter2 < matlab.DiscreteEventSystem
 
     % Discrete-event algorithms
     methods
-        function [entity,events] = entry(obj,storage,entity,source)
-            % 定数
+        function [entity,events] = entry(obj,~,entity,~)
+            % Consumer1の条件 || Consumer2の条件
             if rem(stampEntity() - 1, 7) == 0 || rem(stampEntity() - 5, 7) == 0
                 events = obj.eventForward('output', 1, 1);
             else
@@ -36,17 +37,32 @@ classdef coordinatorRouter2 < matlab.DiscreteEventSystem
     end
 
     methods(Access = protected)
-        function setupImpl(obj)
+        function setupImpl(~)
             % Perform one-time calculations, such as computing constants
         end
 
-        function resetImpl(obj)
-            % Initialize / reset discrete-state properties
+        function num = getNumInputsImpl(~)
+            % Define total number of outputs for system with optional
+            num = 2;
         end
 
         function num = getNumOutputsImpl(~)
             % Define total number of outputs for system with optional
-            num = 1;
+            num = 2;
+        end
+
+        function entityTypes = getEntityTypesImpl(obj)
+            % Return entity type structures with fields specifying
+            % properties such as name, size, data type, and complexity
+            entityTypes = obj.entityType('StartPseudoConsumer');
+        end
+
+        function [storageSpec,I,O] = getEntityStorageImpl(obj)
+            % Return entity storage specification and connectivity
+            % information from input ports and output ports to storage
+            storageSpec = obj.queueFIFO('StartPseudoConsumer', obj.Capacity);
+            I = {1, 1};
+            O = {1, 1};
         end
 
         function hasThreshold = calcThreshold(~)
