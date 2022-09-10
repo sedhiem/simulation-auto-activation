0d377e93e6143908ff76172bf069760256b77070
sedhiem <sedhiem@gmail.com>
2022-09-10 12:49:12 +0900

    Add: sumRequestCount v2 entityServer

--- src/v2/entityServer3.m
+++ src/v2/entityServer3.m
@@ -9,4 +9,7 @@ entity.Data = u;
 utility = addUtility(u);
 entity.Utility = utility;
 coder.extrinsic('fprintf');
+requestCount = sumRequestCount(1);
+fprintf('requestCount: %d\n', requestCount);
+fprintf('utility per requestCount: %d\n', utility/requestCount);
 fprintf('ChainID: %d, u: %d , delay: %d Utility: %d, TimeStamp: %d\n', entity.ChainID, u, delay, utility, stampEntity());
