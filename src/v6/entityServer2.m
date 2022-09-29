ad030103860f36e41ddda20955b89ef541e4f606
sedhiem <sedhiem@gmail.com>
2022-09-29 23:15:58 +0900

    Update: v6 entityServer.m

--- src/v6/entityServer2.m
+++ src/v6/entityServer2.m
@@ -8,9 +8,15 @@ end
 entity.Data = u;
 entity.Utility = u;
 entity.totalUtility = addUtility(u);
+entity.totalUtilityAtRouter2 = addUtilityAtRouter2(u);
 requestCount = sumRequestCount(1);
+requestCountAtRouter2 = sumRequestCountAtRouter2(1);
 entity.utilityPerRequestCount = entity.totalUtility/requestCount;
+entity.utilityPerRequestCountAtRouter2 = entity.totalUtilityAtRouter2/requestCountAtRouter2;
 coder.extrinsic('fprintf');
 fprintf('requestCount: %d\n', requestCount);
+fprintf('requestCountAtRouter2: %d\n', requestCountAtRouter2);
 fprintf('utility per requestCount: %d\n', entity.utilityPerRequestCount);
-fprintf('ChainID: %d, u: %d , delay: %d Utility: %d, TimeStamp: %d\n', entity.ChainID, u, delay, entity.totalUtility, stampEntity());
+fprintf('utility per requestCountAtRouter2: %d\n', entity.utilityPerRequestCountAtRouter2);
+fprintf('ChainID: %d, u: %d , delay: %d utility: %d, TimeStamp: %d\n', entity.ChainID, u, delay, entity.totalUtilityAtRouter2, stampEntity());
+fprintf('ChainID: %d, u: %d , delay: %d totalUtility: %d, TimeStamp: %d\n', entity.ChainID, u, delay, entity.totalUtility, stampEntity());
